9cc6dc3c44f127a21e3f766dd186009d772e6f7d
Hidden-demons <56682231+Hidden-demons@users.noreply.github.com>
2022-11-18 19:23:17 +0800

    初始化

---
+++ server/src/main/java/org/ssh/test/GreetingApplication.java
@@ -0,0 +1,16 @@
+package org.ssh.test;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+@SpringBootApplication
+@EnableConfigurationProperties(GreetingProperties.class)
+public class GreetingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingApplication.class, args);
+	}
+
+}
